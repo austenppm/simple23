edaa788e255ccb1deef6a87374fb6a8891c7c133
kazu44ttaka <99314777+kazu44ttaka@users.noreply.github.com>
2023-04-27 16:49:32 +0900

    miss

--- simple/simple.v
+++ simple/simple.v
@@ -6,6 +6,7 @@
 `include "phasecounter.v"
 `include "ctl.v"
 `include "branch.v"
+`include "RemoveChattering.v"
 
 module simple(
 		input [15:0] in,
@@ -29,7 +30,7 @@ module simple(
 		wire [7:0] d;
 		wire [15:0] exd;
 		wire [15:0] WriteData;
-		wire MemRead,MemWrite,RegWrite,ALUSrc1,ALUSrc2,MemtoReg,Output,Input,ALUorshifter,ce;
+		wire MemRead,MemWrite,RegWrite,ALUSrc1,ALUSrc2,MemtoReg,Output,Input,ALUorshifter,ce,ce1,ce2;
 		wire [15:0] A;
 		wire [15:0] B;
 		wire [15:0] Data1;
@@ -55,12 +56,13 @@ module simple(
 		register MDR(.clk(clk2[0]),.rst_n(rst_n),.WriteData(MDRIn),.DataOut(MDROut));
 		RegisterFile RF(.Read1(Rs),.Read2(Rd),.WriteReg(WriteReg),.WriteData(WriteData),.clk(clk2[0]),.RegWrite(RegWrite),.Data1(Data1),.Data2(Data2));
 		ALU ALU(.ALUctl(ALUctl),.A(A),.B(B),.Out(ALUOut),.Outcond(ALUcondout));
-		ctl ctl(.clk(clk2[3]),.rst_n(rst_n),.inst(Inst),.MemRead(MemRead),.MemWrite(MemWrite),.RegWrite(RegWrite),.ALUSrc1(ALUSrc1),.ALUSrc2(ALUSrc2),.MemtoReg(MemtoReg),.ALUorShifter(ALUorshifter),.Halt(ce),.Output(Output),.Input(Input),.opcode(opcode),.RegDst(RegDst),.Branch(Branch));
+		ctl ctl(.clk(clk2[3]),.rst_n(rst_n),.inst(Inst),.MemRead(MemRead),.MemWrite(MemWrite),.RegWrite(RegWrite),.ALUSrc1(ALUSrc1),.ALUSrc2(ALUSrc2),.MemtoReg(MemtoReg),.ALUorShifter(ALUorshifter),.Halt(ce1),.Output(Output),.Input(Input),.opcode(opcode),.RegDst(RegDst),.Branch(Branch));
 		shifter sf(.A(shifterIn),.opcode(shifterctl),.d(dshift),.Out(shifterOut),.Outcond(shiftercondout));
 		PC PC(.clock(clk2[4]),.reset(rst_n),.branchFlag(brch_sig),.ce(ce),.dr(PCIn),.pc(pc),.pcPlusOne(pcPlusOne));
 		phasecounter a0(.clk(clk),.rst_n(rst_n),.p(clk2));
 		branch br(.cond(SZCVOut),.brch(Branch),.brch_sig(brch_sig));
 		ram ram(.address(pc),.clock(clk2[0]),.data(DROut),.wren(MemWrite));
+		RemoveChattering rc(.clk(clk),.botton(exec),.rst_n(rst_n),.signal(ce2));
 		assign Rs = IROut[13:11];
 		assign Rd = IROut[10:8];
 		assign dshift = IROut[3:0];
@@ -79,5 +81,6 @@ module simple(
 					  DROut;
 		assign exd = (d[7]==1'b0) ? {8'b00000000,d}:
 					  {8'b11111111,d};
+		assign ce = ce1 && ce2;
 endmodule
 		
