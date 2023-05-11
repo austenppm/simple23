fbdd2de1f46db0aa6758a872b6c88c954c556910
kazu44ttaka <kazu44ttaka@gmail.com>
2023-05-11 18:02:26 +0900

    pipeline

--- simple_pipeline/phase3ctl.v
+++ simple_pipeline/phase3ctl.v
@@ -1,22 +1,24 @@
 module phase3ctl(
-	input ALUSrc1in,ALUSrc2in,ALUorshifterin,AS_BCin,
+	input ALUSrc1in,ALUSrc2in,ALUorshifterin,AS_BCin,MemReadin,
 	input [3:0] opcodein,
-	input clk,
-	output ALUSrc1out,ALUSrc2out,ALUorshifterout,AS_BCout,
-	output [3:0] opcodeout);
+	input clk,rst_n,
+	output reg ALUSrc1out,ALUSrc2out,ALUorshifterout,AS_BCout,MemReadout,
+	output reg [3:0] opcodeout);
 	always @ (posedge clk) begin
 		if(!rst_n) begin
 			ALUSrc1out <= 1'b0;
 			ALUSrc2out <= 1'b0;
 			ALUorshifterout <= 1'b0;
 			AS_BCout <= 1'b0;
-			opcodeout <= 47b0000;
+			opcodeout <= 4'b0000;
+			MemReadout <= 1'b0;
 		end else begin
 			ALUSrc1out <= ALUSrc1in;
 			ALUSrc2out <= ALUSrc2in;
 			ALUorshifterout <= ALUorshifterin;
 			AS_BCout <= AS_BCin;
 			opcodeout <= opcodein;
+			MemReadout <= MemReadin;
 		end
 	end
 endmodule
