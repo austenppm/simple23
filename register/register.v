a5a49ced9ad89a1fb91abfbd4ed86d5c80acca7f
Michi0678 <rinki.jinji@gmail.com>
2023-05-11 17:09:17 +0900

    register_is_modified

--- register/register.v
+++ register/register.v
@@ -1,14 +1,16 @@
 module register(
 	input [15:0] WriteData,
-	input clk,rst_n,
+	input clk,rst_n,ce,
 	output reg [15:0] DataOut);
-	reg [15:0] RegData;
 	always @ (posedge clk) begin
 		if(!rst_n) begin
-			RegData <= 16'b0000000000000000;
+			DataOut <= 16'b0000000000000000;
 		end else begin
-			DataOut <= RegData;
-			RegData <= WriteData;
+			if(ce == 1'b1) begin
+				DataOut <= WriteData;
+			end else begin
+				DataOut <= DataOut;
+			end
 		end
 	end
 endmodule
