3f2c392db15fd52afbd756ef9210f4c4c4abd3a0
Michi0678 <rinki.jinji@gmail.com>
2023-05-12 09:49:04 +0900

    ctl_was_modified

--- ctl_pipeline/ctl_pipeline.v
+++ ctl_pipeline/ctl_pipeline.v
@@ -15,7 +15,7 @@ module ctl_pipeline(
 	assign brch_wire = inst[13:11];
 	
 	
-	assign RegWrite = (( twobit == 2'b11 && opcode_wire != 4'b0111 && opcode_wire != 4'b1101 && opcode_wire != 4'b1110 && opcode_wire != 4'b1111 && opcode_wire != 4'b0101) || (twobit == 2'b00 ) || (twobit == 2'b10 && (brch_wire == 3'b000 || brch_wire ==  3'b001))) ? 1'b1:
+	assign RegWrite = (( twobit == 2'b11 && opcode_wire != 4'b0111 && opcode_wire != 4'b1101 && opcode_wire != 4'b1110 && opcode_wire != 4'b1111 && opcode_wire != 4'b0101) || (twobit == 2'b00 ) || (twobit == 2'b10 && (brch_wire == 3'b000 || brch_wire ==  3'b001||brch_wire == 3'b010))) ? 1'b1:
 							1'b0;
 	assign MemWrite = (twobit == 2'b01 ) ? 1'b1:
 							1'b0;
@@ -33,6 +33,8 @@ module ctl_pipeline(
 						1'b0;
 	assign opcode = ( twobit == 2'b11 ) ? opcode_wire:
 						 (twobit == 2'b10 && brch_wire == 3'b000)? 4'b0110:
+						 (twobit == 2'b10 && brch_wire == 3'b010)? 4'b0001:
+						 (twobit == 2'b10 && brch_wire == 3'b011)? 4'b0101:
 						 4'b0000;
 	assign Branch = (twobit == 2'b10 && brch_wire == 3'b111) ? inst[10:8]:
 					  (twobit == 2'b10 && brch_wire == 3'b100) ? brch_wire:
@@ -43,7 +45,7 @@ module ctl_pipeline(
 								 1'b0;
 	assign Halt = (twobit == 2'b11 && opcode_wire == 4'b1111) ? 1'b1:
 						1'b0;
-	assign AS_BC = (twobit == 2'b11  && opcode_wire != 4'b0111 && opcode_wire != 4'b1101 && opcode_wire != 4'b1110 && opcode_wire != 4'b1111 && opcode_wire != 4'b1100) ? 1'b1:
+	assign AS_BC = ((twobit == 2'b11  && opcode_wire != 4'b0111 && opcode_wire != 4'b1101 && opcode_wire != 4'b1110 && opcode_wire != 4'b1111 && opcode_wire != 4'b1100) || (twobit == 2'b10 && brch_wire == 3'b011)) ? 1'b1:
 						1'b0;
 	
 endmodule
